9be4085810db9cc901d48dd97e9725b5a359af9f
exkc <exkc@exkc.moe>
2026-06-19 09:17:08 +0800

    update

--- Readme.md
+++ Readme.md
@@ -29,6 +29,8 @@ Run the fellow command on the TV to replace your home screen
 ```
 
  `access/IMG_5901.JPG` is under CC-BY-SA license
- `access/fallback.png` is proby under Apache License bc it is found in webos oss
-
+ `access/fallback.png` is proby under Apache License bc it is from in webos oss
+ `access/plusjakartasans.woff2` is under  SIL Open Font License
+ `access/appbar.svg` is proby under Apache License bc it is from in https://fonts.google.com/
+ 
  The rest is under  WTFNMFPL.
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 let inited=false;
 let applist;
+let appbardefu;
 	  let noperm=true;
 let appdir='/media/developer/apps/usr/palm/applications/moe.exkc.hoooooooooom';
 function initapplun(){
@@ -12,21 +13,7 @@ function initapplun(){
         permtest.onservicecallback = function(e) {
 	                noperm=(!(JSON.parse(e).errorText === undefined));
 console.log("perm:"+noperm);
-		if (!inited) {
-		 setInterval(() => {
-
-          var brclock = new window.PalmServiceBridge();
-	brclock.onservicecallback= function (e) {
-	let ltime=JSON.parse(e).localtime;
-	
-	clock.innerText=addzero(ltime.year)+'/'+addzero(ltime.month)+'/'+addzero(ltime.day)+' '+addzero(ltime.hour)+':'+addzero(ltime.minute)+':'+ addzero(ltime.second);
-
-};
-	brclock.call('luna://com.palm.systemservice/time/getSystemTime','{}');
-
-    }, 1000);
-}
-		if (noperm){
+			if (noperm){
 			console.log(appdir);
 			if(inited){
 	   bridge.call('luna://org.webosbrew.hbchannel.service/exec','{"command":"luna-send -n 1 -f luna://com.webos.applicationManager/listApps \'{}\'"}');
@@ -54,6 +41,42 @@ permtest.call('luna://com.webos.applicationManager/listApps',"{}");
 
 
 }
+function genappdiv (eachapp,whichappbox) {
+
+		  const appitem = document.createElement("div");
+		  const appname = document.createElement("p");
+		  const appicon = document.createElement("img");
+			if(noperm){
+		appicon.src=appdir+'/hack/'+eachapp.folderPath+'/'+eachapp.icon;
+			} else {
+		appicon.src=eachapp.folderPath+'/'+eachapp.icon;
+			}
+		appicon.setAttribute("class","appicon");
+		appicon.addEventListener("error", function(e){
+			e.target.src=appdir+"/access/fallback.png";
+		});
+		appname.innerText=eachapp.title;
+		appname.setAttribute("class","appname");
+		appitem.setAttribute("class","appitem");
+		appitem.setAttribute("data-appid",eachapp.id);
+		appitem.appendChild(appicon);
+		appitem.appendChild(appname);
+		appitem.addEventListener("click", function(e){
+var brapp=new window.PalmServiceBridge();
+			let clickelm;
+			if (e.target.getAttribute("data-appid")==null){
+				clickelm=e.target.parentElement.getAttribute("data-appid");
+		} else {
+				clickelm=e.target.getAttribute("data-appid");
+
+			}
+		brapp.call('luna://com.webos.service.applicationManager/launch','{"id":"'+clickelm+'"}');
+			
+console.log(clickelm);
+		}); 
+		whichappbox.appendChild(appitem);
+	}
+
 function addzero (l){
 
 
@@ -78,42 +101,21 @@ if (noperm){
 console.log("perm:"+noperm);
 console.log(payload);
 console.log(applist);
-if (inited){appluncher.innerHTML="";}
-		applist.forEach((element) => {
+//barlist=["com.webos.app.camera","com.palm.app.settings"];
+barlist=["com.webos.app.mediadiscovery","com.webos.app.hdmi1","com.webos.app.hdmi2","com.webos.app.hdmi3","com.webos.app.hdmi4","com.webos.app.livetv"];
+if (inited){
 
-		  const appitem = document.createElement("div");
-		  const appname = document.createElement("p");
-		  const appicon = document.createElement("img");
-			if(noperm){
-		appicon.src=appdir+'/hack/'+element.folderPath+'/'+element.icon;
-			} else {
-		appicon.src=element.folderPath+'/'+element.icon;
-			}
-		appicon.setAttribute("class","appicon");
-		appicon.addEventListener("error", function(e){
-			e.target.src=appdir+"/access/fallback.png";
-		});
-		appname.innerText=element.title;
-		appname.setAttribute("class","appname");
-		appitem.setAttribute("class","appitem");
-		appitem.setAttribute("data-appid",element.id);
-		appitem.appendChild(appicon);
-		appitem.appendChild(appname);
-		appitem.addEventListener("click", function(e){
-var brapp=new window.PalmServiceBridge();
-			let clickelm;
-			if (e.target.getAttribute("data-appid")==null){
-				clickelm=e.target.parentElement.getAttribute("data-appid");
-		} else {
-				clickelm=e.target.getAttribute("data-appid");
+	appluncher.innerHTML="";
+} else {
 
-			}
-		brapp.call('luna://com.webos.service.applicationManager/launch','{"id":"'+clickelm+'"}');
-			
-console.log(clickelm);
-		}); 
-		appluncher.appendChild(appitem);
-	});
+barlist.forEach(((eachid) => {
+
+	applist.some((eachapp) => {if ( eachapp.id === eachid) {genappdiv(eachapp,appbar)}} );
+}));
+
+
+}
+		applist.forEach(((echap) => genappdiv(echap,appluncher)));
 	inited=true;
 
 };
@@ -121,6 +123,20 @@ console.log(clickelm);
 
 document.addEventListener("DOMContentLoaded", (event) => {
 initapplun();
+		 setInterval(() => {
+
+          var brclock = new window.PalmServiceBridge();
+	brclock.onservicecallback= function (e) {
+	let ltime=JSON.parse(e).localtime;
+	
+	clocktime.innerText=addzero(ltime.hour)+':'+addzero(ltime.minute)+':'+ addzero(ltime.second);
+	clockdate.innerText=addzero(ltime.year)+'/'+addzero(ltime.month)+'/'+addzero(ltime.day) ;
+
+};
+	brclock.call('luna://com.palm.systemservice/time/getSystemTime','{}');
+
+    }, 1000);
+
 });
 
 document.addEventListener("webOSRelaunch", (event) => {
